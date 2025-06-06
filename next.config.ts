cfaa3c3f5cf00e5e03aa0d19a3468e02531f77ab
icebear0111 <wltjq0901@naver.com>
2025-06-06 21:00:45 +0900

    chore(config): s3 설정 [#7]

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'paydeuk-s3-bucket.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
